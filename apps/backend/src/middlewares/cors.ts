10feeb664972d0e519415362fbf0c8fa2db75c0a
Muhadev <muhammedfayemi@gmail.com>
2025-08-21 12:36:56 +0100

    fixed the cors issues for endpoint

--- apps/backend/src/middlewares/cors.ts
+++ apps/backend/src/middlewares/cors.ts
@@ -1,12 +1,8 @@
 import cors from 'cors';
 
-const allowedOrigins = [
-  'http://localhost:3001',
-  'https://rivy-storefront-monorepo-1.onrender.com',
-];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: true,
   credentials: true,
   optionsSuccessStatus: 200, // Support legacy browsers
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
