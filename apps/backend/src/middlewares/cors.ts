9060fef9e2bb8cc330fe83b8a5631c9b42f905a9
Muhadev <muhammedfayemi@gmail.com>
2025-08-21 14:10:13 +0100

    fixed allowed origin on development

--- apps/backend/src/middlewares/cors.ts
+++ apps/backend/src/middlewares/cors.ts
@@ -1,8 +1,14 @@
 import cors from 'cors';
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://rivy-storefront-monorepo-1.onrender.com',
+  'http://localhost:3001',
+  'http://localhost:3002'
+];
 
 const corsOptions = {
-  origin: true,
+  origin: allowedOrigins,
   credentials: true,
   optionsSuccessStatus: 200, // Support legacy browsers
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
